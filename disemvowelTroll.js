2a3e5ba21f8167d2549f364311af19715dd8e195
Rachel Spencer <rachelspencer688@gmail.com>
2024-01-16 14:24:05 -0500

    solution

--- disemvowelTroll.js
+++ disemvowelTroll.js
@@ -5,3 +5,12 @@
 // Your task is to write a function that takes a string and return a new string with all vowels removed.
 
 // For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+const disemvowel = stirng => {
+    const vowels = ["a", "e", "i", "o", "u", " A", "E", "I", "O", "U"];
+    const noVowels = stirng.split("").filter((letter) => !vowels.includes(letter)).join("");
+    return noVowels;
+};
+
+//TESTS
+console.log(disemvowel("This website is for losers LOL!"))
